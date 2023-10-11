4125428e5caf1a44eb0578f35a40b21721605df4
Frank Connelly <101997670+frankpconnelly@users.noreply.github.com>
2023-10-11 07:59:19 -0500

    Update main.js

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -21,3 +21,14 @@ function simpleMultiplication(number) {
       return(number * 9)
   }
 }
+
+
+/*Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.*/
+
+var countSheep = function (num){
+  let str = "";
+  for(i = 1; i <= num; i++) {str+= `${i} sheep...`}
+  return str;
+}
+
+countSheep(5);
