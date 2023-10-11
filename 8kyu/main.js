d5abaab4584b2d380db4a9bf748d88eb251c4742
Frank Connelly <101997670+frankpconnelly@users.noreply.github.com>
2023-10-10 20:49:43 -0500

    Update main.js

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -5,3 +5,11 @@ function lovefunc(flower1,flower2){
         return true;
     }else return false;
 }
+
+function simpleMultiplication(number) {
+    if(number % 2 == 0) {
+      return(number * 8)
+    }else{
+      return(number * 9)
+  }
+}
